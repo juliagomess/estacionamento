dc597c00b0cee98d41cf7b9565165ebc9ce1de39
Júlia Gomes <juliamgomes1@gmail.com>
2022-09-09 11:28:07 -0300

    quase

--- scripts/Estacionamento.js
+++ scripts/Estacionamento.js
@@ -6,38 +6,73 @@ class Estacionamento {
   }
 
   estacionar(veiculo, horario) {
-    if(this.vagas.length >= 20) {
+    if(this.vagas.length >= this.qntd) {
       alert("Estacionamento cheio");
     } else {
       var novo = {
         veiculo: veiculo,
         horaEntrada: horario,
+        horaSaida: '-',
       };
       this.vagas.push(novo);
-      console.log(vagas);
+      console.log(this.vagas);
     }
   }
 
   liberar(idVeiculo, horario) {
     for (var i = 0; i < this.vagas.length; i++) {
-      if (this.vagas[i].veiculo.getID == idVeiculo) {
-        this.vagas.splice(i, 1);
+      if (this.vagas[i].veiculo.getID() == idVeiculo) {
         var reg = {
           veiculo: this.vagas[i].veiculo,
           horaEntrada: this.vagas[i].horaEntrada,
           horaSaida: horario,
         };
         this.relatorio.push(reg);
+        this.vagas.splice(i, 1);
         break;
       }
     }
+    console.log(this.vagas);
   }
 
   gerarRelatorio() {
-
+    var rel = document.getElementById("relatorio");
+    rel.innerHTML = '';
+    for(var i=0; i<this.relatorio.length; i++) {
+      this.criaDiv(this.relatorio[i]);
+    } 
+    for(var i=0; i<this.vagas.length; i++) {
+      this.criaDiv(this.vagas[i]);
+    }
   }
 
   getSaldo() {
 
   }
+
+  criaDiv(infos) {
+    var div = document.createElement("div");
+    var paragrafoNome = document.createElement("span");
+    var paragrafoPlaca = document.createElement("span");
+    var paragrafoHoraEntrada = document.createElement("span");
+    var paragrafoHoraSaida = document.createElement("span");
+    var nome = document.createTextNode("Nome: " + infos.veiculo.getNome());
+    var placa = document.createTextNode("Placa: " + infos.veiculo.getPlaca());
+    var horaEntrada = document.createTextNode("Hora Entrada: " + infos.horaEntrada);
+    var horaSaida = document.createTextNode("Hora Saida: " + infos.horaSaida);
+  
+    paragrafoNome.append(nome);
+    paragrafoPlaca.append(placa);
+    paragrafoHoraEntrada.append(horaEntrada);  
+    paragrafoHoraSaida.append(horaSaida);
+  
+    div.append(paragrafoNome);
+    div.append(paragrafoPlaca);
+    div.append(paragrafoHoraEntrada);
+    div.append(paragrafoHoraSaida);
+  
+    var rel = document.getElementById("relatorio");
+    rel.append(div);
+  }
+  
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,36 @@
 var estacionamento = new Estacionamento(20);
 var cont = 1;
 
-function entradaVeiculo() {
-  var nome = document.getElementById("nome").value;
-  var placa = document.getElementById("placa").value;
-  var marca = document.getElementById("marca").value;
-  var cor = document.getElementById("cor").value;
-  var horaEntrada = document.getElementById("horaEntrada").value;
+function entradaVeiculo() {  
+  var id = document.getElementById("id");
+  var nome = document.getElementById("nome");
+  var placa = document.getElementById("placa");
+  var marca = document.getElementById("marca");
+  var cor = document.getElementById("cor");
+  var horaEntrada = document.getElementById("horaEntrada");
 
-  var veiculo = new Veiculo(cont,placa,marca,cor,nome);
+  var veiculo = new Veiculo(cont,placa.value,marca.value,cor.value,nome.value);
+  estacionamento.estacionar(veiculo,horaEntrada.value);
+  cont++;
 
-  estacionamento.estacionar(veiculo,horaEntrada);
+  nome.value = '';
+  placa.value = '';
+  marca.value = '';
+  cor.value = '';
+  horaEntrada.value = '';
+  id.value = cont;
+}
+
+function saidaVeiculo() {
+  var id = document.getElementById("idSaida");
+  var horaSaida = document.getElementById("horaSaida");
+
+  estacionamento.liberar(id.value,horaSaida.value);
+
+  id.value = '';
+  horaSaida.value = '';
+}
+
+function relatorio() {
+  estacionamento.gerarRelatorio();
 }
